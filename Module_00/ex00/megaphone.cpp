572582a96c233fb1ccc7ba9192eab57f01ddc937
FirestormPenguin <edoardogiubellini@gmail.com>
2024-03-19 13:52:16 +0100

    ex02

--- Module_00/ex00/megaphone.cpp
+++ Module_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: egiubell <egiubell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 14:35:26 by egiubell          #+#    #+#             */
-/*   Updated: 2024/03/19 11:57:45 by egiubell         ###   ########.fr       */
+/*   Updated: 2024/03/19 12:11:37 by egiubell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,16 @@
 int main(int ac, char **av)
 {
 	int	i;
-	int	j;
 
-	i = 0;
+	i = 1;
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	else
 	{
 		while (av[i])
 		{
-			j = 0;
-			while (av[i][j])
-			{
+			for (int j = 0; av[i][j]; j++)
 				std::cout << (char) toupper(av[i][j]);
-				j++;
-			}
 			if (i < ac - 1)
 				std::cout << ' ';
 			i++;
